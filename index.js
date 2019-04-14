065401236f380515cb9056da50830f6c0f232ef6
amarjeet045 <singhamarjeet0045@gmail.com>
2019-04-15 00:20:14 +0530

    after adding public folder

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/',(req,res) =>{
+/*app.get('/',(req,res) =>{
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
+*/
+
+// to make the static folder means making access all the folder content access 
+app.use(express.static(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT||5000;
 app.listen(PORT, ()=> console.log(`server at ${PORT}`));
